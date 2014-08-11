bfa3a90e832da804b9c7f3433962be26a9fecdd6
LockTar <ralph.jansen@outlook.com>
2014-08-11 21:02:25 +0200

    First version uploaded. Upload files to an ASP .NET MVC Controller.

---
+++ FileManager/FileManager/Controllers/FileController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FileManager.Controllers
+{
+	public class FileController : Controller
+	{
+		public ActionResult Upload(HttpPostedFileBase file)
+		{
+			return Json(new
+			{
+				Success = true,
+				FileName = file.FileName,
+				FileSize = file.ContentLength
+			}, JsonRequestBehavior.AllowGet);
+		}
+	}
+}
